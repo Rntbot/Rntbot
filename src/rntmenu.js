52c4b1881d2942ea0d130a5b829188d6ff443ebb
Rntbot <77173350+Rntbot@users.noreply.github.com>
2021-01-08 18:16:46 -0300

    Add files via upload

---
+++ src/rntmenu.js
@@ -0,0 +1,41 @@
+const rntmenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do RNT:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  TRADUZIDO POR *RNTBOT*
+  DUVIDAS? 👇
+  WA.me/558897966858
+╚════════════════════`
+}
+
+exports.rntmenu = rntmenu
+
+
+
+
+
+
+
+
+
